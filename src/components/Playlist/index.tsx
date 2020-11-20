c0d5571f1d1862d25d728ef5dd09a477e2e221ae
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2020-11-20 11:32:37 -0300

    Remove Effect never used

--- src/components/Playlist/index.tsx
+++ src/components/Playlist/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { searchByName } from '../../utils/searchByName';
-import { useFilter } from '../../hooks/filter';
 
 import {
   Container,
