2a428f07abb7d1b7b1ac2019d40a9e36560ec631
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2020-11-23 01:24:22 -0300

    Change App

--- src/services/api.ts
+++ src/services/api.ts
@@ -11,9 +11,17 @@ export const api = axios.create({
   },
 });
 
+// export const apiMocky = axios.create({
+//   baseURL: 'http://www.mocky.io/v2/5a25fade2e0000213aa90776',
+//   headers: {
+//     Authorization: `Bearer ${access_token}`,
+//   },
+// });
+
 export const apiMocky = axios.create({
   baseURL: 'http://www.mocky.io/v2/5a25fade2e0000213aa90776',
   headers: {
+    Origin: 'https://spotify-test-brs.netlify.app',
     Authorization: `Bearer ${access_token}`,
   },
 });
